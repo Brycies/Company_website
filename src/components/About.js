2d04ce88c20193eb16562ba933b244f8be195200
Brycies <brysonodhiambo29@gmail.com>
2023-08-06 23:27:21 +0300

    scss modifications

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
 function About() {
   return (
     <>
-      <div style={{marginTop:'15rem',width:'100%',height:'10px'}} className="about-scroll"></div>
+      <div
+        style={{ marginTop: "15rem", width: "100%", height: "10px" }}
+        className="about-scroll"
+      ></div>
 
       <div className="container about">
         <div className="row">
@@ -9,11 +12,15 @@ function About() {
             <img alt="about" src="./img/img1.png" className="img-fluid" />
           </div>
           <div className="col-md-6">
-            <h2 className="main-title about-h2">ABOUT</h2>
+            <h2 className="main-title about-h4">ABOUT</h2>
             <p className="main-p">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              quam purus, ullamcorper id risus eu, consectetur consectetur
-              purus. Etiam sagittis in eros ac sollicitudin.
+              At Tech Assist Solutions, we are your reliable partner in
+              navigating the complex world of technology. As a premier tech
+              support company, we specialize in providing comprehensive
+              solutions to all your digital challenges. With a passion for
+              problem-solving and a commitment to excellence, we empower
+              individuals and businesses to harness the full potential of their
+              devices and systems.
             </p>
           </div>
         </div>
